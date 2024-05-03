0d94388de0f8a48d89309f64ceb16ff223055506
Georgen21 <chukwudinwabia42@gmail.com>
2024-05-03 03:25:30 +0100

    Python

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""takes a float a and a float b as arguments and returns their sum."""
+
+
+def add(a: float, b: float) -> float:
+    """sum of two integers."""
+    return a + b
