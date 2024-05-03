3ff7c1edff9c9edfcaf6d773ec3ec9cb0ab3c12a
Georgen21 <chukwudinwabia42@gmail.com>
2024-05-03 03:33:58 +0100

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python3
-"""takes two strings as arguments and concatenates a string"""
+"""takes two strings as arguments and concatenates a string."""
 
 
 def concat(str1: str, str2: str) -> str:
-    """return a concatenated string"""
+    """return a concatenated string."""
     return str1 + str2
